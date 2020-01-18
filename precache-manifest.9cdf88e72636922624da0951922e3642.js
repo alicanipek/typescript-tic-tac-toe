673938414525e20f3b73b457e15b70134344ba57
Ali Can İpek <alicanipek@outlook.com.tr>
2020-01-18 12:03:30 +0300

    Updates

---
+++ precache-manifest.9cdf88e72636922624da0951922e3642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "582ab3b6134020a362350d294859b5c7",
+    "url": "/typescript-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a9a0488d54154417c00a",
+    "url": "/typescript-tic-tac-toe/static/css/main.4548be1d.chunk.css"
+  },
+  {
+    "revision": "0883664fae0038d1a7ce",
+    "url": "/typescript-tic-tac-toe/static/js/2.25e83014.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/typescript-tic-tac-toe/static/js/2.25e83014.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a0488d54154417c00a",
+    "url": "/typescript-tic-tac-toe/static/js/main.ec2f646e.chunk.js"
+  },
+  {
+    "revision": "9c3513a7f03b4f7f796f",
+    "url": "/typescript-tic-tac-toe/static/js/runtime-main.aedeb189.js"
+  }
+]);
